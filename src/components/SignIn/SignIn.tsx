76cf42a4984ee2719170307020556d895c07c2c3
Gee Chai <fc00004@gmail.com>
2024-05-28 12:14:15 +0900

    chore: general clean up of firebase + dependencies

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,48 +1,10 @@
 import Image from "next/image";
-import {
-  getAuth,
-  sendSignInLinkToEmail,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from "firebase/auth";
 import { useState } from "react";
-import { app } from "../../../firebase.config";
 import styles from "./SignIn.module.css";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const auth = getAuth(app);
-
-  const sendEmailLink = () => {
-    const actionCodeSettings = {
-      url: "http://localhost:3000/auth/verifyEmailSignIn",
-      handleCodeInApp: true,
-    };
-
-    sendSignInLinkToEmail(auth, email, actionCodeSettings)
-      .then(() => {
-        window.localStorage.setItem("emailForSignIn", email);
-        setMessage("A sign-in link has been sent to your email address.");
-      })
-      .catch((error) => {
-        console.error(error);
-        setMessage("An error occurred while sending the sign-in link.");
-      });
-  };
-
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        console.log("Google Sign-In result:", result);
-        setMessage("Signed in with Google successfully.");
-      })
-      .catch((error) => {
-        console.error("Error during Google sign-in:", error);
-        setMessage("An error occurred during Google sign-in.");
-      });
-  };
 
   return (
     <div className={styles.signInContainer}>
@@ -53,7 +15,7 @@ const SignIn = () => {
         placeholder="Enter your email"
         className={styles.emailInputField}
       />
-      <button onClick={sendEmailLink} className={styles.signInButton}>
+      {/* <button onClick={sendEmailLink} className={styles.signInButton}>
         <Image
           src="/email-icon.png"
           alt="Email Sign-in Icon"
@@ -62,17 +24,7 @@ const SignIn = () => {
           className={styles.emailIcon}
         />
         <span className={styles.emailSignInText}>Sign in with Email Link</span>
-      </button>
-      <button onClick={signInWithGoogle} className={styles.googleSignInButton}>
-        <Image
-          src="/web_neutral_rd_na.svg"
-          alt="Google Sign-in Logo"
-          width={30}
-          height={30}
-          className={styles.googleIcon}
-        />
-        <span className={styles.googleSignInText}>Sign in with Google</span>
-      </button>
+      </button> */}
       {message && <p className={styles.signInMessage}>{message}</p>}
     </div>
   );
--- src/pages/access.tsx
+++ src/pages/access.tsx
@@ -1,21 +1,13 @@
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { app } from "../../firebase.config";
 import SignIn from "@/components/SignIn/SignIn";
 import UserSection from "@/components/UserSection/UserSection";
 
 const Access: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const auth = getAuth(app);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-
-    return () => unsubscribe();
-  }, [auth]);
-  return <>{user ? <UserSection user={user} /> : <SignIn />}</>;
+  return (
+    <>
+      <h1>Access Component</h1>
+    </>
+  );
 };
 
 export default Access;
