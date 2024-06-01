16494b7a93128198288666267a185d524ddf47c5
Gee Chai <fc00004@gmail.com>
2024-06-01 18:20:46 +0900

    feat: teacher dashboard added

---
+++ src/pages/dashboard/teacher.tsx
@@ -0,0 +1,13 @@
+import SignOutButton from "@/components/SignOutButton/SignOutButton";
+
+const TeacherDashboard = () => {
+  return (
+    <div>
+      <h1>Teacher Dashboard</h1>
+      <SignOutButton />
+      {/* Add teacher-specific components */}
+    </div>
+  );
+};
+
+export default TeacherDashboard;
