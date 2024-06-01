4fed07871a5e26964a167d524ce01c078d08dbe1
Gee Chai <fc00004@gmail.com>
2024-06-01 18:20:21 +0900

    feat: student dashboard added

---
+++ src/pages/dashboard/student.tsx
@@ -0,0 +1,13 @@
+import SignOutButton from "@/components/SignOutButton/SignOutButton";
+
+const StudentDashboard = () => {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+      <SignOutButton />
+      {/* Add student-specific components */}
+    </div>
+  );
+};
+
+export default StudentDashboard;
