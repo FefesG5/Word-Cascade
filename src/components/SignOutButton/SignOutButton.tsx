aa40c5b1caddef8a6693a9b693330e3cdf19fd2b
Gee Chai <fc00004@gmail.com>
2024-06-01 18:18:53 +0900

    fix: SignOut Button to follow new authentication logic

--- src/components/SignOutButton/SignOutButton.tsx
+++ src/components/SignOutButton/SignOutButton.tsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/router";
 
 const SignOutButton = () => {
   const [message, setMessage] = useState("");
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      setMessage("You have successfully signed out.");
+      router.push("/signin");
+    } catch (error) {
+      if (error instanceof Error) {
+        setMessage(error.message);
+      } else {
+        setMessage("An unknown error occurred.");
+      }
+    }
+  };
 
   return (
     <div>
-      {/* <button onClick={signOutUser}>Sign Out</button> */}
+      <button onClick={handleSignOut}>Sign Out</button>
       {message && <p>{message}</p>}
     </div>
   );
