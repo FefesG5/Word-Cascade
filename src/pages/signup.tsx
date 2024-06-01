c0bacba6afe8f7380735ed615576ccca95bfd17b
Gee Chai <fc00004@gmail.com>
2024-06-01 18:23:09 +0900

    feat: signup page to use AuthForm component

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,42 +1,8 @@
 // pages/signup.tsx
-import { useState } from "react";
-import { useAuth } from "@/contexts/AuthContext";
+import AuthForm from "@/components/AuthForm/AuthForm";
 
-const SignUp = () => {
-  const { signUp } = useAuth();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      await signUp(email, password);
-      alert("Sign up successful");
-    } catch (error) {
-      if (error instanceof Error) alert(error.message);
-      else alert("An unknown error occurred");
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Sign Up</button>
-    </form>
-  );
+const SignUpPage = () => {
+  return <AuthForm mode="signUp" />;
 };
 
-export default SignUp;
+export default SignUpPage;
