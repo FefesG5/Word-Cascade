65bd42fecc7fd1e0d0f0d3a50f5a588f0f69bf5b
Gee Chai <fc00004@gmail.com>
2024-06-05 10:57:13 +0900

    feat: Try Angle Kids Logo added to header, reduce size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,11 +27,11 @@ const Header: React.FC = () => {
       <div className={styles.logoContainer}>
         <Link href="/">
           <Image
-            src="/next.svg" // Assuming you have a Next.js logo SVG in your public directory
-            alt="Next.js Logo"
+            src="/try-angle-kids-logo.png"
+            alt="Try Angle Kids Logo"
             className={styles.logo}
-            width={128} // Adjust to the size of your actual logo
-            height={64} // Adjust to the size of your actual logo
+            width={60}
+            height={30}
           />
         </Link>
       </div>
