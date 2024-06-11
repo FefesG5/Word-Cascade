f72efb6b3087f21fc85b3e428b4feac9f96815ad
Gee Chai <fc00004@gmail.com>
2024-06-11 12:20:54 +0900

    feat: User Section component to have default icon and email message

--- src/components/UserSection/UserSection.tsx
+++ src/components/UserSection/UserSection.tsx
@@ -1,7 +1,12 @@
-import { User } from "firebase/auth";
 import styles from "./UserSection.module.css";
 import SignOutButton from "../SignOutButton/SignOutButton";
 import Image from "next/image";
+
+interface User {
+  displayName?: string;
+  email?: string;
+  photoURL?: string;
+}
 
 interface UserSectionProps {
   user: User;
@@ -11,14 +16,14 @@ const UserSection: React.FC<UserSectionProps> = ({ user }) => {
   return (
     <div className={styles.userSection}>
       <Image
-        src={user.photoURL || "/user-icon.svg"}
+        src={user.photoURL || "/person-outline.svg"}
         alt={`${user.displayName || "User"}'s profile`}
         width={50}
         height={50}
         className={styles.profileImage}
       />
       <h3 className={styles.userName}>{user.displayName || "User"}</h3>
-      <p className={styles.userEmail}>{user.email}</p>
+      <p className={styles.userEmail}>{user.email || "No email provided"}</p>
       <SignOutButton />
     </div>
   );
