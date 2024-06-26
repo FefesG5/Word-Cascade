910f9f33068441abd417dc8f063ef0ada8fdf68c
Gee Chai <fc00004@gmail.com>
2024-06-26 10:31:36 +0900

    feat: DashboardLayout apply to all pages under Dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,21 +1,16 @@
 import withAuth from "@/components/WithAuth/WithAuth";
-import DashboardNav from "@/components/DashboardNav/DashboardNav";
+import DashboardLayout from "@/components/DashboardLayout/DashboardLayout";
 import styles from "./Dashboard.module.css";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className={styles.dashboardContainer}>
-      <aside className={styles.sidebar}>
-        <DashboardNav />
-      </aside>
-      <main className={styles.mainContent}>
-        <h1 className={styles.dashboardHeader}>Welcome to the Dashboard</h1>
-        <div className={styles.content}>
-          <h2>Main Section</h2>
-          {/* Add your main content here */}
-        </div>
-      </main>
-    </div>
+    <DashboardLayout>
+      <h1 className={styles.dashboardHeader}>Welcome to the Dashboard</h1>
+      <div className={styles.content}>
+        <h2>Main Section</h2>
+        {/* Add your main content here */}
+      </div>
+    </DashboardLayout>
   );
 };
 
