63a84723c4ba341e5362f20f9f071185f8c41296
Gee Chai <fc00004@gmail.com>
2024-06-26 10:32:07 +0900

    feat: DashboardLayout to apply to word-cascade page

--- src/pages/dashboard/word-cascade.tsx
+++ src/pages/dashboard/word-cascade.tsx
@@ -1,11 +1,12 @@
 import WithAuth from "@/components/WithAuth/WithAuth";
+import DashboardLayout from "@/components/DashboardLayout/DashboardLayout";
 
 const WordCascade: React.FC = () => {
   return (
-    <div>
+    <DashboardLayout>
       <h1>Welcome to the Word Cascade Page</h1>
       {/* Add your Word Cascade feature components here */}
-    </div>
+    </DashboardLayout>
   );
 };
 
