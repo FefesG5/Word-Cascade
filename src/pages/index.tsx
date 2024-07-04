82930139616f8065a8f0d66cf33986d627023a27
Gee Chai <fc00004@gmail.com>
2024-07-04 09:42:03 +0900

    refactor: removed list item

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,6 @@ export default function Home() {
             </li>
             <li className={styles.featureItem}>Expand vocabulary</li>
             <li className={styles.featureItem}>Fun and interactive gameplay</li>
-            <li className={styles.featureItem}>Progress tracking</li>
           </ul>
         </section>
 
